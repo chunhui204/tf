29c00fa7bfe55373280636c78162c608f8f1b2ba
chunhui204 <15866613796@163.com>
2017-12-23 13:52:43 +0800

    Update next_batch.py

--- next_batch.py
+++ next_batch.py
@@ -3,18 +3,18 @@ import numpy as np
 #mnist_train_labels: training labels
 #
 
-start = 0
+end = 0
 def next_batch(batch_size):
-    global start
+    global end
     global mnist_train_images
     global mnist_train_labels
     
-    start = start+batch_size
+    end = end+batch_size
     #if finish an epoch, permutate data randomly
-    if start >= mnist_train_images.shape[0]:
-        start = 0
+    if end - batch_size > mnist_train_images.shape[0]:
+        end = batch_size
         # not essential to specify a seed for permutation, np.random.permutation has different results every time
         permutation = np.random.permutation(mnist_train_images.shape[0])
         mnist_train_images = mnist_train_images[permutation,:]
         mnist_train_labels = mnist_train_labels[permutation,:]
-    return mnist_train_images[start:start+batch_size,:], mnist_train_labels[start:start+batch_size,:]
+    return mnist_train_images[end-batch_size:end,:], mnist_train_labels[end-batch_size:end,:]
